fbdeb1762e802c2fad0dfdd1d2e4c3ff9b8b5512
raquel <raquelfrancisco587@gmail.com>
2022-12-03 15:59:30 -0300

    JOGOS

---
+++ ex1.js
@@ -0,0 +1,12 @@
+let opcao= "";
+let palavra= "";
+do{
+opcao= ""
+palavra= prompt( "digite a palavra");
+ console.log (`a hora do${palavra}`);
+ console.log( `o {palavra} de OZ`);
+ console.log(`11 homens e um ${palavra}`);
+ console.log(`${palavra} dos macacos`);
+ console.log(`procurando ${palavra}`);
+
+} while(opcao)
